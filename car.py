4f4d77b008b131194d8b651b3280f14f38b71288
CaptainMumo <mumopeter17@gmail.com>
2018-11-24 19:07:05 +0300

    Updated __init__ method to take only name, model and car type arguments

--- car.py
+++ car.py
@@ -1,18 +1,18 @@
 class Car:
 
-    def __init__(self, name = "General", model = "GM", car_type = "saloon", speed = 0, num_of_doors = 4, num_of_wheels = 4):
+    def __init__(self, name = "General", model = "GM", car_type = "saloon"):
         self.name = name
         self.model = model
         self.car_type = car_type
-        self.speed = speed
+        self.speed = 0
 
         if self.name == "Porsche" or self.name == "Koenigsegg":
             self.num_of_doors = 2
         else:
-            self.num_of_doors = num_of_doors
+            self.num_of_doors = 4
         
         if self.car_type == "saloon":
-            self.num_of_wheels = num_of_wheels
+            self.num_of_wheels = 4
         else:
             self.num_of_wheels = 8
     
